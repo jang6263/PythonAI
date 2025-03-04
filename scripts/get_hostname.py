79dadc370b004483fc1068eb057d7f067cf2f0bd
jang6263 <89028021+jang6263@users.noreply.github.com>
2025-03-04 11:18:57 +0900

    Update get_hostname.py

--- scripts/get_hostname.py
+++ scripts/get_hostname.py
@@ -6,15 +6,14 @@ def get_hostname():
 
 def on_ui_tabs():
     with gr.Blocks() as demo:
-        gr.Markdown("## 호스트네임 확인")
-        output = gr.Textbox(label="호스트네임")
-        btn = gr.Button("호스트네임 가져오기")
+        gr.Markdown("## Hostname")
+        output = gr.Textbox(label="Hostname")
+        btn = gr.Button("Hostname")
         btn.click(get_hostname, outputs=output)
-    return [(demo, "호스트네임 확장", "hostname_extension")]
+    return [(demo, "Hostname", "hostname_extension")]
 
-# Stable Diffusion WebUI에서 확장 프로그램을 인식하도록 함
 try:
     import modules.scripts as scripts
     scripts.script_callbacks.on_ui_tabs(on_ui_tabs)
 except ImportError:
-    print("Stable Diffusion WebUI에서 실행해야 합니다.")
+    print("You Must Run Stable Diffusion WebUI")
